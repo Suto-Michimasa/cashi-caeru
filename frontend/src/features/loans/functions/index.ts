13c3a9ec38c05abeee5729ad2825f4b0a19189e5
Suto <sutou1335michimasa@gmail.com>
2023-06-15 17:43:35 +0900

    add createLoan call

--- frontend/src/features/loans/functions/index.ts
+++ frontend/src/features/loans/functions/index.ts
@@ -3,6 +3,10 @@ import { CreateLoanRequestBody } from "../types";
 import { functions } from "@/plugins/firebase";
 
 const createLoanCallable = httpsCallable(functions, 'createLoan');
+const completePaymentCallable = httpsCallable(functions, 'completePayment');
 
 export const createLoan = (req: CreateLoanRequestBody) =>
   createLoanCallable(req)
+
+export const completePayment = (paymentId: string) =>
+  completePaymentCallable({ paymentId })
