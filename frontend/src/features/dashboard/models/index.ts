bb11f7bda58f8812119cd935e2e9a367c611db20
Suto <sutou1335michimasa@gmail.com>
2023-06-15 16:33:21 +0900

    update test

--- frontend/src/features/dashboard/models/index.ts
+++ frontend/src/features/dashboard/models/index.ts
@@ -1,9 +1,9 @@
 import { CreateLoanRequestBody } from "@/features/loans/types";
 
 export const requestLoanData: CreateLoanRequestBody  = {
-  borrowerId: 'U1234567890',
-  lenderId: 'U0987654321',
+  borrowerId: 'Uc6ed8bbaaa81cea19b12f98d022c86de',
+  lenderId: 'Ucd6450f6f50b65dfa706c18012015039',
   description: 'お金を貸してください',
-  amount: 1000,
-  deadline: new Date(),
+  amount: 2000,
+  deadline: new Date(2023, 6, 30)
 };
